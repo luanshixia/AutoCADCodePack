fe16f72b5e2b91e8a9f5a7aa79ac207d2092c722
lavantgarde <subjectivelyobjective10@gmail.com>
2018-06-19 21:42:34 -0700

    Implemented flat riemman sum approximation
    Works on planar-curves on the Y-Axis.

--- AutoCADCommands/Algorithms.cs
+++ AutoCADCommands/Algorithms.cs
@@ -454,68 +454,84 @@ namespace AutoCADCommands
         }
 
 
-        public enum RiemmanType
+        public enum RiemmanSumRule
         {
             Left = 1, Right = 2, Middle = 3
         }
 
-        public static Rectangle3d[] RiemmanSumRectangles(this Curve upperboundCurve, Line lowerbound, RiemmanType riemmanType, int intervals)
+        /// <summary>
+        /// Gets an array of rectangles used in a Riemman sum approximation.
+        /// </summary>
+        /// <param name="upperboundCurve">The curve to be approximated.</param>
+        /// <param name="lowerbound">The line defining the range under the upperbound curve.</param>
+        /// <param name="riemmanSumRule">The approximation rule.</param>
+        /// <param name="intervals">The number of intervals, or rectangles, to use. Also the number of rectangles to be returned.</param>
+        /// <returns></returns>
+        public static Rectangle3d[] RiemmanSumRectangles(this Curve upperboundCurve, Line lowerbound, RiemmanSumRule riemmanSumRule, int intervals)
         {
+            var rectangles = new Rectangle3d[intervals];
+
             var x = lowerbound.EndPoint.DistanceTo(lowerbound.StartPoint) / intervals;
-            var rectangles = new List<Rectangle3d>(intervals);
 
-            //switch (riemmanType)
-            //{
-            //    case RiemmanType.Left:
-            //    {
-            //        break;
-            //    }
-            //    case RiemmanType.Right:
-            //    {
-            //        break;
-            //    }
-            //    case RiemmanType.Middle:
-            //    {
-            //        break;
-            //    }
-            //}
-
-            var i = 1;
             for (var k = 0; k < intervals; k++)
             {
+                // p2 p3
+                // p1 p4
 
-                // x2 x3
-                // x1 x4
-
-
-                var x1 = new Point3d(
+                var p1 = new Point3d(
                     x: lowerbound.StartPoint.X + (x * k),
-                    y: lowerbound.StartPoint.Y * k,
-                    z: lowerbound.StartPoint.Z * k);
+                    y: lowerbound.StartPoint.Y,
+                    z: lowerbound.StartPoint.Z);
 
-                var x4 = new Point3d(
-                    x: lowerbound.StartPoint.X + (x * i),
-                    y: lowerbound.StartPoint.Y * i,
-                    z: lowerbound.StartPoint.Z * i);
+                var p4 = new Point3d(
+                    x: lowerbound.StartPoint.X + (x * (k + 1)),
+                    y: lowerbound.StartPoint.Y,
+                    z: lowerbound.StartPoint.Z);
 
-                var x2Line = new Line(x1, new Point3d(x1.X, upperboundCurve.GeometricExtents.MaxPoint.Y + 1, x4.Z));
-                var x3Line = new Line(x4, new Point3d(x4.X, upperboundCurve.GeometricExtents.MaxPoint.Y + 1, x4.Z));
+                var p2Intersect = new Line(p1, new Point3d(p1.X, upperboundCurve.GeometricExtents.MaxPoint.Y + 1, p1.Z));
+                var p3Intersect = new Line(p4, new Point3d(p4.X, upperboundCurve.GeometricExtents.MaxPoint.Y + 1, p4.Z));
 
-                var x2 = upperboundCurve.Intersect(x2Line, Autodesk.AutoCAD.DatabaseServices.Intersect.ExtendArgument)[0];
-                var x3 = upperboundCurve.Intersect(x3Line, Autodesk.AutoCAD.DatabaseServices.Intersect.ExtendArgument)[0];
+                var p2Intersection = upperboundCurve.Intersect(p2Intersect, Autodesk.AutoCAD.DatabaseServices.Intersect.ExtendArgument)?[0]; // y = f(x0)
+                var p3Intersection = upperboundCurve.Intersect(p3Intersect, Autodesk.AutoCAD.DatabaseServices.Intersect.ExtendArgument)?[0]; // y = f(x1)
 
-                i++;
+                var p2 = p2Intersection ?? throw new ArgumentOutOfRangeException();
+                var p3 = p3Intersection ?? throw new ArgumentOutOfRangeException();
 
+                switch (riemmanSumRule)
+                {
+                    case RiemmanSumRule.Left:
+                        {
+                            p3 = new Point3d(p3.X, p2.Y, p3.Z);
+                            break;
+                        }
+                    case RiemmanSumRule.Right:
+                        {
+                            p2 = new Point3d(p2.X, p3.Y, p2.Z);
+                            break;
+                        }
+                    case RiemmanSumRule.Middle:
+                        {
+                            var midx = (p4.X - p1.X) / 2;
+                            var midLine = new Line(new Point3d(p1.X + midx, p1.Y, p1.Z), new Point3d(p1.X + midx, upperboundCurve.GeometricExtents.MaxPoint.Y + 1, p1.Z));
 
-                var rectangle = new Rectangle3d(x2, x3, x1, x4);
-                rectangles.Add(rectangle);
+                            var midIntersection = upperboundCurve.Intersect(midLine, Autodesk.AutoCAD.DatabaseServices.Intersect.ExtendArgument)?[0];
+
+                            var intersect = midIntersection ?? throw new ArgumentOutOfRangeException();
+
+                            p2 = new Point3d(p1.X, intersect.Y, p1.Z);
+                            p3 = new Point3d(p4.X, intersect.Y, p4.Z);
+
+                            break;
+                        }
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(riemmanSumRule), riemmanSumRule, null);
+                }
+                var rectangle = new Rectangle3d(p2, p3, p1, p4);
+                rectangles[k] = rectangle;
             }
-
-    
 
             return rectangles.ToArray();
         }
-
 
         /// <summary>
         /// Gets all points on curve whose parameters are integers.
